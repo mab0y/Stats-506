6321cfde665a0928957e75e3596c0dbf30363f5f
mab0y <mab0y@outlook.com>
2023-09-20 17:29:54 -0400

    Problem 2 Finished

--- PS-02/Problem 2 - Linear Regression.R
+++ PS-02/Problem 2 - Linear Regression.R
@@ -10,7 +10,13 @@ cars$year <- as.factor(cars$year)
 cars_lm <- lm(highway_mpg ~ horsepower + torque + height + length + width + year, cars)
 
 cars_lm_inter <- lm(highway_mpg ~ horsepower * torque +  year, cars)
-
 interact_plot(cars_lm_inter, pred = horsepower, modx = torque, at = list("year"=factor(2011)))
 
-              
+horsepower_torque<-cars$horsepower*cars$torque
+year2010 <- as.numeric(cars$year == 2010)
+year2011 <- as.numeric(cars$year == 2011)              
+year2012 <- as.numeric(cars$year == 2012)
+
+design_matrix <- cbind(rep(1,nrow(cars)),cars[,c("horsepower","torque")],year2010,year2011,year2012,horsepower_torque)
+design_matrix <- as.matrix(design_matrix)
+beta_hat <- solve(t(design_matrix) %*% design_matrix) %*% t(design_matrix) %*% cars$highway_mpg
